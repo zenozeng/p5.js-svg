3b88fdd1f7fcb5e35ebe5b02b8318a4de45d6267
Zeno Zeng <zenoofzeng@gmail.com>
2015-05-19 11:56:35 +0800

    update testRender

--- test/test.js
+++ test/test.js
@@ -11,6 +11,13 @@ $(function() {
         p5svg.background(255);
         p5canvas.background(255);
 
+        var fnbody = draw.toString();
+        fnbody = fnbody.substring(fnbody.indexOf('{') + 1, fnbody.lastIndexOf('}'));
+        [p5svg, p5canvas].forEach(function(p) {
+            with (p) {
+                eval(fnbody);
+            }
+        });
         draw(p5svg);
         draw(p5canvas);
 
@@ -33,11 +40,19 @@ $(function() {
         match = svgpng === canvaspng ? 'fa-check-circle' : 'fa-times-circle';
         $('#tests').append('<div class="match"><i class="fa ' + match + '"></i></div>');
 
-        $('#tests').append('<div class="function">' + draw.toString() + '</div>');
-        $('#tests').append('<br>');
+        $('#tests').append('<div class="function">' + fnbody.replace(/;/g, ';<br>') + '</div>');
+        $('#tests').append('<br><br>');
     };
 
-    testRender(function(p) {
-        p.ellipse(50, 50, 50, 25);
+    testRender(function() {
+        ellipse(50, 50, 50, 25);
+    });
+
+    testRender(function() {
+        line(30, 20, 85, 20);
+        stroke(126);
+        line(85, 20, 85, 75);
+        stroke(255);
+        line(85, 75, 30, 75);
     });
 });
