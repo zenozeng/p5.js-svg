7faa4d89599d567ae301c0c6b57594d93b8b7f88
Zeno Zeng <zenoofzeng@gmail.com>
2015-07-09 23:58:03 +0800

    update draft/await

--- draft/await.js
+++ draft/await.js
@@ -8,6 +8,20 @@ p5.prototype._await = function(fn) {
 
     // pause following functions
 
+    var collect = {};
+
+    var queue = [];
+
+    // note: functions outside p5 will not wait!
+    for (p in p5.prototype) {
+        if (typeof p === "function") {
+            collect[p] = p5.prototype[p];
+            p5.prototype[p] = function() {
+                queue.push({fn: p, args: arguments});
+            };
+        }
+    }
+
     fn().then(function() {
         p._loop = _loop;
         // resume following functions
