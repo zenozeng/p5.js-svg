d08783b35733062c82582835bc35e2095a560ffc
Zeno Zeng <zenoofzeng@gmail.com>
2015-08-12 21:20:40 +0800

    add p5.prototype.filter, fixes #103

--- demo/demo.js
+++ demo/demo.js
@@ -4,9 +4,7 @@ function setup() {
     ellipse(width/2, height/2, 50, 50);
     ellipse(width/2, height/2, 50, 50);
     frameRate(10);
-    var elt = querySVG('g path')[0].parentNode();
-    elt.filter(BLUR, 5);
-    elt.unfilter(BLUR, 5);
+    filter(BLUR, 5);
     noLoop();
 }
 
--- src/filters.js
+++ src/filters.js
@@ -4,12 +4,27 @@ module.exports = function(p5) {
     var SVGFilters = require('./p5.SVGFilters')(p5);
 
     var _filter = p5.prototype.filter;
-    p5.prototype.filter = function(opreation, value) {
+    p5.prototype.filter = function(operation, value) {
         if (this._graphics.svg) {
             // move nodes to a new <g>
             var nodes = this._graphics.svg.children;
+            var g = p5.SVGElement.create('g');
+            this._graphics._setGCFlag(g.elt);
+            this._graphics.svg.appendChild(g.elt);
             for (var i = 0; i < nodes.length; i++) {
+                var node = nodes[i];
+                if (node !== g.elt && (node.nodeName.toLowerCase() !== 'defs')) {
+                    g.append(node);
+                }
             }
+
+            // apply filter
+            g.filter(operation, value);
+
+            // create new <g> so that new element won't be influenced by the filter
+            g = p5.SVGElement.create('g');
+            this._graphics._setGCFlag(g.elt);
+            this._graphics.svg.appendChild(g.elt);
         } else {
             _filter.apply(this, arguments);
         }
