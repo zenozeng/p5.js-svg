4a3e6248659cbd131db134fd5696f98b5cc1ea14
Zeno Zeng <zenoofzeng@gmail.com>
2021-06-14 15:00:15 +0800

    test: add test for #176

--- test/unit/io/save.js
+++ test/unit/io/save.js
@@ -1,8 +1,30 @@
-import {p5} from '../../lib';
+import {p5, assert} from '../../lib';
 import {testDownload} from './test-download';
 
 describe('IO/save', function() {
     this.timeout(1000 * 5);
+
+    // See https://github.com/zenozeng/p5.js-svg/issues/176
+    it('should generate valid svg output', async function() {
+        const dataURL = await new Promise((resolve) => {
+            new p5((p) => {
+                p.setup = function () {
+                    p.createCanvas(600, 600, p.SVG);
+                    p.downloadFile = function(dataURL) {
+                        resolve(dataURL)
+                    }
+                }
+
+                p.draw = function() {
+                    p.rect(0, 0, 100, 100);
+                    p.line(30, 20, 85, 75);
+                    p.save();
+                    p.noLoop();
+                }
+            })
+        });
+        assert.equal(dataURL.indexOf('#'), -1);
+    })
 
     it('save()', function(done) {
         testDownload('untitled', 'svg', function(p) {
