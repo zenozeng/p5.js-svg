b2d037dc26bae420d30d9a4ef32ad3eb28fa17d3
Zeno Zeng <zenoofzeng@gmail.com>
2021-10-05 15:01:09 +0800

    style: make eslint happy

--- src/index.js
+++ src/index.js
@@ -9,19 +9,19 @@ function init(p5) {
     /**
      * @namespace p5
      */
-     RendererSVG(p5);
-     Rendering(p5);
-     IO(p5);
-     Element(p5);
-     Filters(p5);
+    RendererSVG(p5);
+    Rendering(p5);
+    IO(p5);
+    Element(p5);
+    Filters(p5);
 
-     // attach constants to p5 instance
-     Object.keys(constants).forEach(function(k) {
-         p5.prototype[k] = constants[k];
-     });
+    // attach constants to p5 instance
+    Object.keys(constants).forEach(function(k) {
+        p5.prototype[k] = constants[k];
+    });
 }
 
-if (typeof window.p5 !== "undefined") {
+if (typeof window.p5 !== 'undefined') {
     init(window.p5);
 }
 
