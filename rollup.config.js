b62ce7275b0642bddf7417ab1a00a7100a8d54d0
Zeno Zeng <zenoofzeng@gmail.com>
2022-05-08 19:41:29 +0800

    fix: add cjs output

--- rollup.config.js
+++ rollup.config.js
@@ -2,15 +2,27 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve'
 
 export default [
+    // unpkg
     {
         input: 'src/index.js',
         output: {
             file: 'dist/p5.svg.js',
             format: 'iife',
-            sourcemap: true            
+            sourcemap: true
         },
         plugins: [resolve(), commonjs()]
     },
+    // cjs (webpack, vite)
+    {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/p5.svg.cjs.js',
+            format: 'cjs',
+            sourcemap: true
+        },
+        plugins: [resolve(), commonjs()]
+    },
+    // test
     {
         input: 'test/unit/index.js',
         output: {
