553c2b28a9309de98f333364b980f7c7d6bfaaf9
Zeno Zeng <zenoofzeng@gmail.com>
2023-06-24 19:54:44 +0800

    feat: examples/p5-svelte

---
+++ examples/p5-svelte/src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import P5 from "p5-svelte";
+  import * as p5 from "p5";
+  import init from "p5.js-svg";
+
+  init(p5);
+
+  let width = 55;
+  let height = 55;
+
+  const sketch = (p5) => {
+    p5.setup = () => {
+      p5.createCanvas(400, 400, p5.SVG);
+    };
+
+    p5.draw = () => {
+      p5.ellipse(p5.width / 2, p5.height / 2, width, height);
+    };
+  };
+</script>
+
+<label>
+  Width
+  <input type="range" bind:value={width} min="100" max="1000" step="0.01" />
+  {width}
+</label>
+
+<label>
+  Height
+  <input type="range" bind:value={height} min="100" max="1000" step="0.01" />
+  {height}
+</label>
+
+<P5 {sketch} />
